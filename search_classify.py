661598d0ed33c275e4aaccdef027d1d0cc12a72f
Sai Bhargava Ramu <saiediitm@gmail.com>
2017-07-19 13:54:27 +0530

    for suggestion
    YCrCb couldn't detect any car image and RGB HSV HLS detect concentrated rail guard along with car ... so much of false positives

---
+++ search_classify.py
@@ -0,0 +1,151 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import glob
+import time
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+from skimage.feature import hog
+from scipy.ndimage.measurements import label
+
+from lesson_functions import *
+# NOTE: the next import is only valid for scikit-learn version <= 0.17
+# for scikit-learn >= 0.18 use:
+# from sklearn.model_selection import train_test_split
+from sklearn.cross_validation import train_test_split
+    
+    
+# Read in cars and notcars
+cars = []
+for filename in glob.iglob(r'data\vehicles\**\*.png'):
+    cars.append(filename)
+    
+notcars = []
+for filename in glob.iglob(r'data\non-vehicles\**\*.png'):
+    notcars.append(filename)
+        
+# Reduce the sample size because
+# The quiz evaluator times out after 13s of CPU time
+
+sample_size = 500
+cars = cars[0:sample_size]
+notcars = notcars[0:sample_size]
+
+
+### TODO: Tweak these parameters and see how the results change.
+color_space = 'HSV' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+orient = 9  # HOG orientations
+pix_per_cell = 8 # HOG pixels per cell
+cell_per_block = 2 # HOG cells per block
+hog_channel = 2 # Can be 0, 1, 2, or "ALL"
+spatial_size = (16, 16) # Spatial binning dimensions
+hist_bins = 16    # Number of histogram bins
+spatial_feat = True # Spatial features on or off
+hist_feat = True # Histogram features on or off
+hog_feat = True # HOG features on or off
+y_start_stop = [400, 656] # Min and max in y to search in slide_window()
+
+t = time.time()
+car_features = extract_features(cars[:], color_space=color_space, 
+                        spatial_size=spatial_size, hist_bins=hist_bins, 
+                        orient=orient, pix_per_cell=pix_per_cell, 
+                        cell_per_block=cell_per_block, 
+                        hog_channel=hog_channel, spatial_feat=spatial_feat, 
+                        hist_feat=hist_feat, hog_feat=hog_feat)
+notcar_features = extract_features(notcars[:], color_space=color_space, 
+                        spatial_size=spatial_size, hist_bins=hist_bins, 
+                        orient=orient, pix_per_cell=pix_per_cell, 
+                        cell_per_block=cell_per_block, 
+                        hog_channel=hog_channel, spatial_feat=spatial_feat, 
+                        hist_feat=hist_feat, hog_feat=hog_feat)
+
+print(round(time.time()-t, 2), 'Seconds to extract features...',color_space,hog_channel)
+
+X = np.vstack((car_features, notcar_features)).astype(np.float64)                        
+# Fit a per-column scaler
+#print('***',X.shape)
+X_scaler = StandardScaler().fit(X)
+# Apply the scaler to X
+scaled_X = X_scaler.transform(X)
+
+# Define the labels vector
+y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+
+
+# Split up data into randomized training and test sets
+rand_state = np.random.randint(0, 100)
+X_train, X_test, y_train, y_test = train_test_split(
+    scaled_X, y, test_size=0.2, random_state=rand_state)
+
+print('Using:',orient,'orientations',pix_per_cell,
+    'pixels per cell and', cell_per_block,'cells per block')
+print('Feature vector length:', len(X_train[0]))
+# Use a linear SVC 
+svc = LinearSVC()
+# Check the training time for the SVC
+t=time.time()
+svc.fit(X_train, y_train)
+t2 = time.time()
+print(round(t2-t, 2), 'Seconds to train SVC...')
+# Check the score of the SVC
+print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+# Check the prediction time for a single sample
+t=time.time()
+
+image = mpimg.imread('test1.jpg')
+draw_image = np.copy(image)
+
+# Uncomment the following line if you extracted training
+# data from .png images (scaled 0 to 1 by mpimg) and the
+# image you are searching is a .jpg (scaled 0 to 255)
+#image = image.astype(np.float32)/255
+
+##windows = slide_window(image, x_start_stop=[None, None], y_start_stop=y_start_stop, 
+##                    xy_window=(96, 96), xy_overlap=(0.7, 0.7))
+
+pyramid = [((64, 64),  [400, 500]),
+           ((96, 96),  [400, 500]),
+           ((80, 80),  [450, 550]),
+           ((100, 100),[450, 550]),
+           ((128, 128),[450, 650]),]
+
+windows = create_windows(pyramid, image, xy_overlap=(0.01,0.01))
+
+print('****',len(windows),type(windows))
+
+hot_windows = search_windows(image, windows, svc, X_scaler, color_space=color_space, 
+                        spatial_size=spatial_size, hist_bins=hist_bins, 
+                        orient=orient, pix_per_cell=pix_per_cell, 
+                        cell_per_block=cell_per_block, 
+                        hog_channel=hog_channel, spatial_feat=spatial_feat, 
+                        hist_feat=hist_feat, hog_feat=hog_feat)                       
+
+window_img = draw_boxes(draw_image, windows, color=(0, 0, 255), thick=6)                    
+
+plt.imshow(window_img)
+plt.show()
+
+heat = np.zeros_like(image[:,:,0]).astype(np.float)
+# Add heat to each box in box list
+heat = add_heat(heat,hot_windows)
+    
+# Apply threshold to help remove false positives
+heat = apply_threshold(heat,1)
+
+# Visualize the heatmap when displaying    
+heatmap = np.clip(heat, 0, 255)
+
+# Find final boxes from heatmap using label function
+labels = label(heatmap)
+draw_img = draw_labeled_bboxes(np.copy(image), labels)
+
+fig = plt.figure()
+plt.subplot(121)
+plt.imshow(draw_img)
+plt.title('Car Positions')
+plt.subplot(122)
+plt.imshow(heatmap, cmap='hot')
+plt.title('Heat Map')
+fig.tight_layout()
+plt.show()
